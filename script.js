18e994e7c8da56bed47cbd0be429d0172094d035
AlexisUriel394255 <uclastle9112342@gmail.com>
2025-11-19 11:14:12 -0600

    Se corrigio un error
    Updated loop to start from 1 instead of 0 and removed duplicate FizzBuzz check.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
- for (let i = 0; i <=1000; i++) {
+ for (let i = 1; i <=1000; i++) {
 /*El bucle for con la variable i que representa el
 numero que se comienza desde 0 hasta 1000*/ 
+    if (i % 3 ==0 && i % 5 ==0) {
+    console.log("FizzBuzz");
+    /*Si el numero es divisible entre 3 y 5
+    se imprime en consola FizzBuzz */
+  }
   if (i % 3 == 0) {
     console.log("Fizz");
 /*Si el numero que es la variable i es divisible por 3
@@ -10,11 +15,6 @@ se imprime en consola Fizz */
     console.log("Buzz");
 /*o si el numero que es la variable i es divisible por 5
 se imprime en consola Buzz */
-  }
-  if (i % 3 ==0 && i % 5 ==0) {
-    console.log("FizzBuzz");
-    /*Si el numero es divisible entre 3 y 5
-    se imprime en consola FizzBuzz */
   }
   //Si no se cumplio ninguna condicion solo se imprime el numero
   console.log(i);
